4280abcaecfcb323f03a074ccb3ee0b923436ee2
Chhavi0104 <chhavi074@gmail.com>
2026-05-14 12:41:43 +0530

    Integrated OCR with capture module

---
+++ core/ocr_engine.py
@@ -0,0 +1,20 @@
+import paddleocr
+
+ocr = paddleocr.PaddleOCR(use_angle_cls=True, lang='en')
+
+def extract_text(image):
+    if image is None:
+        print("No image received for OCR.")
+        return ""
+
+    results = ocr.ocr(image)
+
+    detected_text = []
+
+    for line in results[0]:
+        text = line[1][0]
+        detected_text.append(text)
+
+    final_text = " ".join(detected_text)
+
+    return final_text
